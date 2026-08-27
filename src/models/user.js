d1a844800d1b566240fec7a7eb6b37e6aba72327
Bautista Berenfeld <bbautista2026ipf@gmail.com>
2026-08-27 16:06:46 -0300

    feat: implementar controladores CRUD de User y Task con validaciones

--- src/models/user.js
+++ src/models/user.js
@@ -1,31 +1,31 @@
-import { DataTypes } from "sequelize";
-import db from "../config/database.js";
+import { DataTypes } from 'sequelize';
+import db from '../config/database.js';
 
 const User = db.define(
-  "User",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+    'User',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
     },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-  },
-  {
-    timestamps: false,
-  },
+    {
+        timestamps: false,
+    }
 );
 
 export default User;
