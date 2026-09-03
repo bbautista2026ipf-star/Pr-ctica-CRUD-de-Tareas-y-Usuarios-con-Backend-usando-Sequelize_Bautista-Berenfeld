cd963da5a8127659d266d86457a9513e7a257f75
Bautista Berenfeld <bbautista2026ipf@gmail.com>
2026-09-03 16:11:37 -0300

    fix: se restauro la versión validada de controladores y rutas

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,13 +6,19 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/taskController.js";
+import { validate } from "../middlewares/validate.js";
+import {
+  createTaskValidation,
+  updateTaskValidation,
+  idParamValidation,
+} from "../middlewares/taskValidations.js";
 
 const router = Router();
 
-router.post("/", createTask);
+router.post("/", createTaskValidation, validate, createTask);
 router.get("/", getTasks);
-router.get("/:id", getTaskById);
-router.put("/:id", updateTask);
-router.delete("/:id", deleteTask);
+router.get("/:id", idParamValidation, validate, getTaskById);
+router.put("/:id", updateTaskValidation, validate, updateTask);
+router.delete("/:id", idParamValidation, validate, deleteTask);
 
 export default router;
